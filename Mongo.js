bd72aea9d4f76ebe4475ca867bf4f9f38734ca99
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-12-16 09:24:55 +0530

    Update Mongo.js

--- Mongo.js
+++ Mongo.js
@@ -22,29 +22,41 @@ const client = new MongoClient(url);
 
 const dbName = "HelloWorld";
 
+
 async function main() {
-  await client.connect();
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection("User");
+        await client.connect();
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+        const collection = db.collection("User");
+
+        const data ={
+          firstname : "Dodla",
+          lastname  : "Sha"
+        };
+
+
+      //INSERT
+      const insertResult = await collection.insertMany([data]);
+      console.log('Inserted documents =>', insertResult);
+      // READ
+      const findResult = await collection.find({}).toArray();
+      console.log('Found documents =>', findResult);
+      //COUNT
+      const countDocuments = await collection.countDocuments({});
+      console.log('Total documents =>', countDocuments);
+      // Find all documents with a filter of firstName
+      const firstName = await collection.find({"firstname" : "Dev"}).toArray();
+      console.log('Found documents =>', firstName);
+      //Count all documents with a filter of firstName
+      const count = await collection.countDocuments({ firstname: "Dev" });
+      console.log('Count of documents with firstname "Dev" =>', count);
+
 
 
   
-const data = {
-  firstname : "Dev",
-  lastname  : "Sharma"
-};
-//INSERT
-const insertResult = await collection.insertMany([data]);
-console.log('Inserted documents =>', insertResult);
- // READ
-const findResult = await collection.find({}).toArray();
-console.log('Found documents =>', findResult);
-
-
-  
-  return "done.";
+ return "done.";
 }
+
 
 main()
   .then(console.log)
